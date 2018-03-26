756a3fcb85f74e5a1c6172c583e4f9e52850efef
JoeyDrab <ccc7.com@gmail.com>
2018-03-27 00:16:52 +0200

    ...

---
+++ 2.Year/IFJ/FreeBasicCompiler/src/syntax.h
@@ -0,0 +1,24 @@
+//__________________________________________//
+//******************************************//
+//part:		IFJ/ syntax.h					//
+//team members: M. Kascak, J. Drab, 		//
+//				Ch. Farkas, V. Cervenanska	//
+//written by: J.Drab						//
+//__________________________________________//
+
+#ifndef _SYNTAX_H_
+#define _SYNTAX_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void parser_if();
+void parser_loop();
+void parser_if_code();
+void parser_loop_code();
+void parser_scope();
+void parser_input();
+void parser_func();
+void parser_dim();
+
+#endif
